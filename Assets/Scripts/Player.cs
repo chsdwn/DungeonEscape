4ec550213bf71fe0ff830c79e3226762537e60a4
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-09-04 16:22:52 +0300

    8. Challenge

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float velocity = Input.GetAxisRaw("Horizontal");
+        GetComponent<Rigidbody2D>().velocity = new Vector2(velocity, 0);
+    }
+}
