f96059a09e25ade6f5d9e0b5eb1896eeb19f60b6
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-09-04 17:13:32 +0300

    10. Challenge - Player Jumping

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,21 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class Player : MonoBehaviour
 {
-    Rigidbody2D rb;
+    [SerializeField]
+    float jumpForce = 4f;
+
+    Rigidbody2D rb2D;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (IsGrounded())
+            {
+                rb2D.AddForce(Vector2.up * jumpForce);
+                Debug.Log("jump");
+            }
+        }
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        float velocity = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(velocity, 0);
+        float move = Input.GetAxisRaw("Horizontal");
+
+        rb2D.velocity = new Vector2(move, 0);
+    }
+
+    bool IsGrounded()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up, .6f, LayerMask.GetMask("Floor"));
+
+        if (hit.collider != null)
+            return true;
+
+        return false;
     }
 }
