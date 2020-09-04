3fea9e34fde8146dbe89780f7e17ad010481a15e
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-09-04 16:27:52 +0300

    9. Challenge Review

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         float velocity = Input.GetAxisRaw("Horizontal");
-        GetComponent<Rigidbody2D>().velocity = new Vector2(velocity, 0);
+        rb.velocity = new Vector2(velocity, 0);
     }
 }
