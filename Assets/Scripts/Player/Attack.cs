ed154f6a471def234939665f57cf6ca75bcbd972
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-09-08 17:32:56 +0300

    11-12. Challenge Review - Skeleton Damage Cooldown

--- Assets/Scripts/Player/Attack.cs
+++ Assets/Scripts/Player/Attack.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class Attack : MonoBehaviour
 {
+    bool CanDamage = true;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         IDamageable hit = other.GetComponent<IDamageable>();
 
         if (hit != null)
         {
-            hit.Damage();
+            if (CanDamage)
+            {
+                hit.Damage();
+                CanDamage = false;
+                Debug.Log("Hit");
+                StartCoroutine(Wait500msUntilHitAgainCoroutine());
+                CanDamage = true;
+            }
         }
+    }
+
+    IEnumerator Wait500msUntilHitAgainCoroutine()
+    {
+        yield return new WaitForSeconds(0.5f);
     }
 }
