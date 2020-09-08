e7613fe0389a8d708e3bc2e695df67d0e843ee76
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-09-08 12:48:16 +0300

    9. Challenge - Moss Giant Movement

--- Assets/Scripts/Enemy/MossGiant.cs
+++ Assets/Scripts/Enemy/MossGiant.cs
@@ -4,6 +4,23 @@ using UnityEngine;
 
 public class MossGiant : Enemy
 {
+    Vector3 target;
+
     public override void Attack() { }
-    public override void Update() { }
+    public override void Update()
+    {
+
+        if (Mathf.Approximately(transform.position.x, pointA.position.x))
+        {
+            target = pointB.position;
+            GetComponentInChildren<SpriteRenderer>().flipX = false;
+        }
+        else if (Mathf.Approximately(transform.position.x, pointB.position.x))
+        {
+            target = pointA.position;
+            GetComponentInChildren<SpriteRenderer>().flipX = true;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+    }
 }
