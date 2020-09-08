b911701825564dde885d764e6e2697187274dd62
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-09-08 13:56:23 +0300

    21. Challenge - Spider AI

--- Assets/Scripts/Enemy/Spider.cs
+++ Assets/Scripts/Enemy/Spider.cs
@@ -4,9 +4,46 @@ using UnityEngine;
 
 public class Spider : Enemy
 {
+    Animator anim;
+    SpriteRenderer spriteRenderer;
+    Vector3 target;
+
     void Start()
     {
-
+        anim = GetComponentInChildren<Animator>();
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
     }
-    public override void Update() { }
+    public override void Update()
+    {
+        if (IsAnimationPlaying("Idle"))
+            return;
+
+        Movement();
+    }
+
+    void Movement()
+    {
+        if (Mathf.Approximately(transform.position.x, pointA.position.x))
+        {
+            target = pointB.position;
+            anim.SetTrigger("Idle");
+            spriteRenderer.flipX = false;
+        }
+        else if (Mathf.Approximately(transform.position.x, pointB.position.x))
+        {
+            target = pointA.position;
+            anim.SetTrigger("Idle");
+            spriteRenderer.flipX = true;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+    }
+
+    bool IsAnimationPlaying(string name)
+    {
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName(name))
+            return true;
+
+        return false;
+    }
 }
