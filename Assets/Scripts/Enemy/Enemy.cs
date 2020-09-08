c000dfd8a372f45569b33f493f0190e1c0d1409c
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-09-08 14:21:22 +0300

    23. Enemy Class Shared Behaviour

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -13,10 +13,52 @@ public abstract class Enemy : MonoBehaviour
     [SerializeField]
     protected Transform pointA, pointB;
 
-    public virtual void Attack()
-    {
+    protected Animator anim;
+    protected SpriteRenderer spriteRenderer;
+    protected Vector3 target;
 
+    void Start()
+    {
+        Init();
     }
 
-    public abstract void Update();
+    protected virtual void Update()
+    {
+        if (IsAnimationPlaying("Idle"))
+            return;
+
+        Movement();
+    }
+
+    protected virtual void Init()
+    {
+        anim = GetComponentInChildren<Animator>();
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    protected void Movement()
+    {
+        if (Mathf.Approximately(transform.position.x, pointA.position.x))
+        {
+            target = pointB.position;
+            spriteRenderer.flipX = false;
+            anim.SetTrigger("Idle");
+        }
+        else if (Mathf.Approximately(transform.position.x, pointB.position.x))
+        {
+            target = pointA.position;
+            spriteRenderer.flipX = true;
+            anim.SetTrigger("Idle");
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+    }
+
+    protected bool IsAnimationPlaying(string name)
+    {
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName(name))
+            return true;
+
+        return false;
+    }
 }
